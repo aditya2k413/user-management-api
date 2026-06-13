1582e0e01b037508a1622626d16b1064b713ba69
Aditya Adhikari <aditya.2k413@gmail.com>
2026-06-13 23:27:42 +0530

    implement repository layer

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	db "UserAgeAPI/db/sqlc/generated"
+	"context"
+)
+
+type UserRepository struct {
+	queries *db.Queries
+}
+
+func NewUserRepository(queries *db.Queries) *UserRepository {
+	return &UserRepository{
+		queries: queries,
+	}
+}
+
+func (r *UserRepository) CreateUser(
+	ctx context.Context,
+	params db.CreateUserParams,
+) (db.User, error) {
+
+	return r.queries.CreateUser(ctx, params)
+}
+
+func (r *UserRepository) GetUser(
+	ctx context.Context,
+	id int32,
+) (db.User, error) {
+	return r.GetUser(ctx, id)
+
+}
+
+func (r *UserRepository) ListUsers(
+	ctx context.Context,
+) ([]db.User, error) {
+
+	return r.queries.ListUsers(ctx)
+}
+
+func (r *UserRepository) UpdateUser(
+	ctx context.Context,
+	params db.UpdateUserParams,
+) (db.User, error) {
+
+	return r.queries.UpdateUser(ctx, params)
+}
+
+func (r *UserRepository) DeleteUser(
+	ctx context.Context,
+	id int32,
+) error {
+
+	return r.queries.DeleteUser(ctx, id)
+}
