114322b4a1b60ad1f426f2460a63c233869e90f5
Aditya Adhikari <aditya.2k413@gmail.com>
2026-06-13 23:17:59 +0530

    add user models

---
+++ internal/models/user.go
@@ -0,0 +1,18 @@
+package models
+
+type CreateUserRequest struct {
+	Name string `json:"name" validate:"required,min=2,max=100"`
+	Dob  string `json:"dob" validate:"required"`
+}
+
+type UpdateUserRequest struct {
+	Name string `json:"name" validate:"required,min=2,max=100"`
+	Dob  string `json:"dob" validate:"required"`
+}
+
+type UserResponse struct {
+	ID   int32  `json:"id"`
+	Name string `json:"name"`
+	Dob  string `json:"dob"`
+	Age  int    `json:"age,omitempty"`
+}
